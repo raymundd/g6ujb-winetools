858d711fd35010ee18d532b4b508b35b4eeb5bc1
raymundd <rayd.9100@gmail.com>
2021-08-07 17:04:27 +0300

    Create start_user_tool.sh

---
+++ start_user_tool.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#cp ~/Flexradio/SSDR_RDX6500.settings "/home/raymundd/radiotools/drive_c/users/raymundd/AppData/Roaming/FlexRadio Systems/SSDR.settings"
+env WINEPREFIX=$HOME/radiotools wine control.exe
